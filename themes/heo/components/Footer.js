95f48195d3ff303b42da8f36db7d71c3c0430ab0
Winner365 <133582008+Winner365-China@users.noreply.github.com>
2024-06-04 11:46:15 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -27,8 +27,8 @@ const Footer = ({ title }) => {
       <div
         id='footer-bottom'
         className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
-        <div id='footer-bottom-left'>
-          NotionNext {siteConfig('VERSION')} <i className='fas fa-copyright' />{' '}
+        <div id='footer-bottom-left'> Powered by
+          Notion {siteConfig('VERSION')} <i className='fas fa-copyright' />{' '}
           {`${copyrightDate}`} <i className='mx-1 animate-pulse fas fa-heart' />{' '}
           <a
             href={siteConfig('LINK')}
