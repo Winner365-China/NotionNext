a584e0b0575c451f6a0a5a6956dce4eec9a3aa2d
Winner365 <133582008+Winner365-China@users.noreply.github.com>
2024-06-28 09:59:16 +0800

    Update PostCopyright.js

--- themes/heo/components/PostCopyright.js
+++ themes/heo/components/PostCopyright.js
@@ -21,10 +21,8 @@ export default function PostCopyright() {
   if (!siteConfig('HEO_ARTICLE_COPYRIGHT', null, CONFIG)) {
     return <></>
   }
-  // 判断“copyright”是否为空
- if (frontMatter?.copyright === '' || frontMatter?.copyright === null) {
-   // 如果为空，返回默认版权信息
-      return (
+
+  return (
     <section className='dark:text-gray-300 mt-6 mx-1 '>
       <ul className='overflow-x-auto whitespace-nowrap text-sm dark:bg-gray-900 bg-gray-100 p-5 leading-8 border-l-2 border-indigo-500'>
         <li>
@@ -47,31 +45,4 @@ export default function PostCopyright() {
         </li>
       </ul>
     </section>
-  );
-  }
-  // 否则返回自定义信息
-    return (
-      <section className='dark:text-gray-300 mt-6 mx-1 '>
-      <ul className='overflow-x-auto whitespace-nowrap text-sm dark:bg-gray-900 bg-gray-100 p-5 leading-8 border-l-2 border-indigo-500'>
-        <li>
-          <strong className='mr-2'>{locale.COMMON.AUTHOR}:</strong>
-          <Link href={'/about'} className='hover:underline'>
-            {siteConfig('AUTHOR')}
-          </Link>
-        </li>
-        <li>
-          <strong className='mr-2'>{locale.COMMON.URL}:</strong>
-          <a
-            className='whitespace-normal break-words hover:underline'
-            href={path}>
-            {path}
-          </a>
-        </li>
-        <li>
-          <strong className='mr-2'>{locale.COMMON.COPYRIGHT}:</strong>
-          {frontMatter?.copyright}
-        </li>
-      </ul>
-    </section>
-          );
-}
+  )
